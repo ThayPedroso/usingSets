5e688a26a9ba3988078c3a965f9b1a30d1f11b36
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-02-11 10:36:50 -0300

    solution 1

--- usingSets/Program.cs
+++ usingSets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace usingSets
 {
@@ -6,7 +7,40 @@ namespace usingSets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+            HashSet<int> ALL = new HashSet<int>();
+
+            Console.WriteLine("Total students in course a: ");
+            int qtdA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtdA; i++)
+            {
+                int stdA = int.Parse(Console.ReadLine());
+                A.Add(stdA);
+            }
+
+            Console.WriteLine("Total students in course b: ");
+            int qtdB = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtdB; i++)
+            {
+                int stdB = int.Parse(Console.ReadLine());
+                B.Add(stdB);
+            }
+
+            Console.WriteLine("Total students in course c: ");
+            int qtdC = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtdC; i++)
+            {
+                int stdC = int.Parse(Console.ReadLine());
+                C.Add(stdC);
+            }
+
+            ALL.UnionWith(A);
+            ALL.UnionWith(B);
+            ALL.UnionWith(C);
+
+            Console.WriteLine("Total students: " + ALL.Count);
         }
     }
 }
